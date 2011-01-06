11be31edd31562b2a585f4fc2c314acb7f0af751
Peter Schuller <peter.schuller@infidyne.com>
2011-01-06 02:21:58 +0100

    Skeleton.

---
+++ src/main/clojure/org/scode/lrugctest.clj
@@ -0,0 +1,11 @@
+(ns org.scode.lrugctest
+  (:gen-class)
+  (:require [clojure.contrib.duck-streams :as duck-streams])
+  (:use compojure.core
+        compojure.response
+        ring.adapter.jetty))
+
+(defn -main [& args]
+  (println "todo"))
+
+
